884059211887a85314e308b01b0e637a37895e63
sosng <sosngg@gmail.com>
2018-11-01 18:01:35 +0800

    update transformations

--- GL003-transformation/GL003-transformation/GLView.swift
+++ GL003-transformation/GL003-transformation/GLView.swift
@@ -11,15 +11,44 @@ import GLKit
 
 class GLView: UIView {
 
-    var context: CVEAGLContext?
-    
+    private var context: CVEAGLContext?
+    private var renderBuffer = GLuint()
+    private var frameBuffer = GLuint()
+    private var vao = GLuint()
+    private var vbo = GLuint()
+    private var shader: Shader!
     override class var layerClass: AnyClass {
         return CAEAGLLayer.self
     }
     
+    private var degree: Float = 0.0
+    private var yDegree: Float = 0.0
+    private var bX = false
+    private var bY = false
+    private var timer: Timer?
+    
+    
+    private let incides = [0, 3, 2,
+                           0, 1, 3,
+                           0, 2, 4,
+                           0, 4, 1,
+                           2, 3, 4,
+                           1, 4, 3]
+    
+    private let vertices = [Vertext(-0.5, 0.5, 0.0, 1.0, 0.0, 1.0),
+                            Vertext(0.5, 0.5, 0.0, 1.0, 0.0, 1.0),
+                            Vertext(-0.5, -0.5, 0.0, 1.0, 1.0, 1.0),
+                            Vertext(0.5, -0.5, 0.0, 1.0, 1.0, 1.0),
+                            Vertext(0.0, 0.0, 1.0, 0.0, 1.0, 0.0),]
+    
     override func layoutSubviews() {
         setupLayer()
         setupContext()
+        destoryRenderBufferAndFrameBuffer()
+        setupRenderBuffer()
+        setupFrameBuffer()
+        setupShader()
+        render()
     }
     
     
@@ -35,5 +64,144 @@ class GLView: UIView {
         EAGLContext.setCurrent(context)
     }
     
+    private func destoryRenderBufferAndFrameBuffer() {
+        glDeleteFramebuffers(1, &frameBuffer)
+        frameBuffer = 0
+        glDeleteRenderbuffers(1, &renderBuffer)
+        renderBuffer = 0
+    }
+    
+    private func setupRenderBuffer() {
+        glGenRenderbuffers(1, &renderBuffer)
+        glBindRenderbuffer(GLenum(GL_RENDERBUFFER), renderBuffer)
+        context?.renderbufferStorage(Int(GL_RENDERBUFFER), from: self.layer as? CAEAGLLayer)
+    }
+    
+    private func setupFrameBuffer() {
+        glGenFramebuffers(1, &frameBuffer)
+        glBindFramebuffer(GLenum(GL_FRAMEBUFFER), frameBuffer)
+        glFramebufferRenderbuffer(GLenum(GL_FRAMEBUFFER),
+                                  GLenum(GL_COLOR_ATTACHMENT0),
+                                  GLenum(GL_RENDERBUFFER),
+                                  renderBuffer)
+    }
+    
+    private func setupShader() {
+        shader = Shader(vertexShader: "shaderv.vsh", fragShader: "shaderf.fsh")
+    }
+    
+    private func render() {
+        glClearColor(0.0, 0.0, 0.0, 1.0)
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT))
+        
+        let scale = UIScreen.main.scale
+        glViewport(GLint(frame.origin.x * scale),
+                   GLint(frame.origin.y * scale),
+                   GLint(frame.size.width * scale),
+                   GLint(frame.size.height * scale))
+        
+        shader.prepareDraw()
+        
+//        glGenVertexArraysOES(1, &vao)
+//        glBindVertexArrayOES(vao)
 
+        glGenBuffers(1, &vbo)
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), vbo)
+        glBufferData(GLenum(GL_ARRAY_BUFFER),
+                     vertices.size,
+                     vertices,
+                     GLenum(GL_DYNAMIC_DRAW))
+        glBindBuffer(GLenum(GL_ARRAY_BUFFER), vbo)
+        
+        // shader attribute
+        let postition = shader.attributeLocation("position")
+        glVertexAttribPointer(postition,
+                              3,
+                              GLenum(GL_FLOAT),
+                              GLboolean(GL_FALSE),
+                              GLsizei(MemoryLayout<Vertext>.stride),
+                              nil)
+        glEnableVertexAttribArray(postition)
+        
+        let color = shader.attributeLocation("positionColor")
+        glVertexAttribPointer(color,
+                              3,
+                              GLenum(GL_FLOAT),
+                              GLboolean(GL_FALSE),
+                              GLsizei(MemoryLayout<Vertext>.stride),
+                              UnsafeRawPointer(bitPattern: MemoryLayout<Vertext>.stride * 3))
+    
+        let pojectionMatrixSlot = shader.unifromLocation("projectionMatrix")
+        let modelViewMatrixSlot = shader.unifromLocation("modelViewMatrix")
+        
+        let width = frame.size.width
+        let height = frame.size.height
+        let aspect = Float(width / height)
+        
+        // 透视变换
+        var projectionMatrix = KSMatrix4()
+        ksMatrixLoadIdentity(&projectionMatrix)
+        
+        // 透视变换.视角30°
+        ksPerspective(&projectionMatrix, 30.0, aspect, 5.0, 20.0)
+        
+        // 设置glsl里面的投影矩阵
+        glUniformMatrix4fv(GLint(pojectionMatrixSlot),
+                           1,
+                           GLboolean(GL_FALSE),
+                           &projectionMatrix.m.0.0)
+        glEnable(GLenum(GL_CULL_FACE))
+        
+        // 模型
+        var modelViewMatrix = KSMatrix4()
+        ksMatrixLoadIdentity(&modelViewMatrix)
+        
+        // 平移
+        ksTranslate(&modelViewMatrix, 0.0, 0.0, -10.0)
+        // 旋转
+        var rotationMatrix = KSMatrix4()
+        ksMatrixLoadIdentity(&rotationMatrix)
+        ksRotate(&rotationMatrix, degree, 1.0, 0.0, 0.0) // X 轴
+        ksRotate(&rotationMatrix, yDegree, 0.0, 1.0, 0.0) // Y 轴
+        
+        // 将变换矩阵相乘
+        var temp = modelViewMatrix
+        ksMatrixMultiply(&temp, &rotationMatrix, &modelViewMatrix)
+        
+        //
+        glUniformMatrix4fv(GLint(modelViewMatrixSlot),
+                           1,
+                           GLboolean(GL_FALSE),
+                           &modelViewMatrix.m.0.0)
+        
+        glDrawElements(GLenum(GL_TRIANGLES),
+                       GLsizei(incides.count),
+                       GLenum(GL_UNSIGNED_INT),
+                       incides)
+        
+        context?.presentRenderbuffer(Int(GL_RENDERBUFFER))
+    }
+    
+    @IBAction func tapX(_ sender: Any) {
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(onRes), userInfo: nil, repeats: true)
+        }
+        bX = !bX;
+    }
+    
+    @IBAction func tapY(_ sender: Any) {
+        if timer == nil {
+            timer = Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(onRes), userInfo: nil, repeats: true)
+        }
+        bY = !bY
+    }
+    
+}
+
+extension GLView {
+    @objc func onRes() {
+        degree += (bX ? 1 : 0) * 5
+        yDegree += (bY ? 1 : 0) * 5
+        render()
+    }
 }
---
+++ GL003-transformation/GL003-transformation/Shader.swift
@@ -0,0 +1,130 @@
+//
+//  Shader.swift
+//  GL-EBO
+//
+//  Created by naver on 2018/10/30.
+//  Copyright © 2018 naver. All rights reserved.
+//
+
+import Foundation
+import GLKit
+
+class Shader {
+    
+    private var programHandle = GLuint()
+    
+    init(vertexShader: String, fragShader: String) {
+        programHandle = compile(vertextShader: vertexShader, fragShader: fragShader)
+    }
+    
+    func prepareDraw() {
+        glUseProgram(programHandle)
+    }
+    
+    func attributeLocation(_ name: String) -> GLuint {
+        return GLuint(glGetAttribLocation(programHandle, name))
+    }
+    
+    func unifromLocation(_ name: String) -> GLuint {
+        return GLuint(glGetUniformLocation(programHandle, name))
+    }
+    
+    private func compile(vertextShader: String, fragShader: String) -> GLuint {
+        // 1
+        let vertexShaderName = compile(shader: vertextShader, type: GLenum(GL_VERTEX_SHADER))
+        let fragmentShaderName = compile(shader: fragShader, type: GLenum(GL_FRAGMENT_SHADER))
+        // 2
+        let program = glCreateProgram()
+        // 3
+        glAttachShader(program, vertexShaderName)
+        glAttachShader(program, fragmentShaderName)
+        //
+        glLinkProgram(program)
+        //
+        var linkStatus = GLint()
+        glGetProgramiv(program, GLenum(GL_LINK_STATUS), &linkStatus)
+        if linkStatus == GL_FALSE {
+            var infoLength : GLsizei = 0
+            let bufferLength : GLsizei = 1024
+            glGetProgramiv(program, GLenum(GL_INFO_LOG_LENGTH), &infoLength)
+            
+            let info : [GLchar] = Array(repeating: GLchar(0), count: Int(bufferLength))
+            var actualLength : GLsizei = 0
+            
+            glGetProgramInfoLog(program, bufferLength, &actualLength, UnsafeMutablePointer(mutating: info))
+            print("=============\nshader link status info: \(String(describing: String(validatingUTF8: info)))\n=============")
+            
+            exit(0)
+        }
+        
+        return program
+    }
+    
+    private func compile(shader: String, type: GLenum) -> GLuint {
+        guard let path = Bundle.main.path(forResource: shader, ofType: nil) else {
+            exit(0)
+        }
+        do {
+            // 1 - 读取shaders string
+            let shaderString = try String(contentsOfFile: path, encoding: .utf8)
+            // 2 - handler
+            let shaderHandler = glCreateShader(type)
+            // 3 -
+            var shaderStringLength = GLint(Int32(shaderString.lengthOfBytes(using: .utf8)))
+            let shaderCString = shaderString.cString(using: .utf8)
+            var pointer = UnsafePointer<GLchar>(shaderCString)
+            // 4 -
+            glShaderSource(shaderHandler, GLsizei(1), &pointer, &shaderStringLength)
+            // 5 -
+            glCompileShader(shaderHandler)
+            // 6 -
+            var compileStatus = GLint(0)
+            glGetShaderiv(shaderHandler, GLenum(GL_COMPILE_STATUS), &compileStatus)
+            
+            if compileStatus == GL_FALSE {
+                var infoLength: GLsizei = 0
+                var bufferLength: GLsizei = 1024
+                
+                glGetShaderiv(shaderHandler, GLenum(GL_INFO_LOG_LENGTH), &infoLength)
+                let info = Array(repeating: GLchar(0), count: Int(bufferLength))
+                
+                var actualLength: GLsizei = 0
+                glGetShaderInfoLog(shaderHandler, bufferLength, &actualLength, UnsafeMutablePointer(mutating: info))
+                
+                print("=============\nshader compile status info: \(String(describing: String(validatingUTF8: info)))\nshader string:\(shaderString)=============")
+                exit(0)
+                
+            }
+            
+            return shaderHandler
+        } catch {
+            exit(0)
+        }
+        
+    }
+    
+}
+
+struct Vertext {
+    var x: GLfloat
+    var y: GLfloat
+    var z: GLfloat
+    var r: GLfloat
+    var g: GLfloat
+    var b: GLfloat
+    
+    init(_ x: GLfloat, _ y: GLfloat, _ z: GLfloat, _ r: GLfloat, _ g: GLfloat, _ b: GLfloat) {
+        self.x = x
+        self.y = y
+        self.z = z
+        self.r = r
+        self.g = g
+        self.b = b
+    }
+}
+
+extension Array {
+    var size: Int {
+        return MemoryLayout<Element>.stride * count
+    }
+}
